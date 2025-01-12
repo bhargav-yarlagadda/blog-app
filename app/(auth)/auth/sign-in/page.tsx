c78d8a5b495d38709f7cc49bd061539fc8ac38eb
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-01-12 20:08:38 +0530

    Sign-In enabled

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+"use client";
+import React, { useContext, useState } from "react";
 import Link from "next/link";
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { AuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 const SignInPage = () => {
+  const router = useRouter();
+  const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount();
+  const [error, setError] = useState("");
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { checkAuthUser, isLoading: isUserLoading } = useContext(AuthContext);
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault(); // Prevent default form submission
+    try {
+      const session = await signInAccount(formValues);
+      if (session) {
+        await checkAuthUser(); // Check if the user is authenticated
+        router.push("/"); // Redirect to the dashboard
+      }else{
+
+      setError("Invalid email or password. Please try again.");
+      }
+    } catch (err) {
+      setError("Invalid email or password. Please try again.");
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black ">
+    <div className="min-h-screen flex items-center justify-center bg-black">
       <div className="w-full max-w-md shadow-lg rounded-lg p-8">
         <h2 className="text-3xl font-bold text-white text-center mb-6">
           Welcome Back to <br />
-           <p className="text-cyan-400">BlogVerse</p>
+          <p className="text-cyan-400">BlogVerse</p>
         </h2>
-        <form className="space-y-4">
+        {error && (
+          <div className="text-red-400">
+          {error}
+        </div>)}
+        <form className="space-y-4" onSubmit={handleSubmit}>
           {/* Email Input */}
           <div>
             <label
@@ -24,6 +58,12 @@ const SignInPage = () => {
               className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="example@domain.com"
               required
+              onChange={(e) =>
+                setFormValues((prev) => ({
+                  ...prev,
+                  email: e.target.value,
+                }))
+              }
             />
           </div>
           {/* Password Input */}
@@ -40,6 +80,12 @@ const SignInPage = () => {
               className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="••••••••"
               required
+              onChange={(e) =>
+                setFormValues((prev) => ({
+                  ...prev,
+                  password: e.target.value,
+                }))
+              }
             />
           </div>
           {/* Remember Me and Forgot Password */}
@@ -48,7 +94,7 @@ const SignInPage = () => {
               <input
                 type="checkbox"
                 id="remember-me"
-                className="h-4 w-4 bg-gray-700 border border-gray-600 rounded "
+                className="h-4 w-4 bg-gray-700 border border-gray-600 rounded"
               />
               <label
                 htmlFor="remember-me"
@@ -67,15 +113,16 @@ const SignInPage = () => {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 rounded-lg shadow-lg transition duration-300"
+            className={`w-full ${
+              isSigningIn ? "bg-gray-500" : "bg-blue-600 hover:bg-blue-500"
+            } text-white font-semibold py-2 rounded-lg shadow-lg transition duration-300`}
+            disabled={isSigningIn}
           >
-            Sign In
+            {isSigningIn ? "Signing In..." : "Sign In"}
           </button>
         </form>
         {/* Divider */}
         <div className="text-center my-4 text-gray-500">OR</div>
-        {/* Social Login */}
-      
         {/* Footer */}
         <p className="text-sm text-center text-gray-400 mt-4">
           Don't have an account?{" "}
