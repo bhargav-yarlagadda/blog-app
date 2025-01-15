0e8241dec342b96fd6296630dc0470ab2b70ab03
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-01-15 18:31:48 +0530

    chore

---
+++ app/(profile)/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = async ({params}:{params:{id:string}}) => {
+    const id = (await params).id
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default page
